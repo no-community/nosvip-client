2ef66900d688d0403a5301e2e3549ecff16441e6
tfcye <whoisye@outlook.com>
2020-09-20 23:29:23 +0800

    update dist

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routerHistory = createWebHistory()
+const routerHistory = createWebHashHistory()
 
 const router = createRouter({
     history: routerHistory,
